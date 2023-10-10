c09963276e80f29963f945fd98b016f259153e63
Declan Michaels <declan.michaels.actor@gmail.com>
2023-10-10 13:30:04 -0500

    new streamlined fizzbuzzer

---
+++ index.js
@@ -0,0 +1,19 @@
+
+const fizzbuzzer = require('./fizzbuzzer')
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+const main = async () => {
+      
+      readline.question('What is your top number? ', topNumber => {
+        topNumber++
+        console.log('topNumber is: ', topNumber)
+        for (let x = 1; x<topNumber; x++) {
+            console.log(fizzbuzzer(x));
+        }
+        readline.close();
+      });
+}
+main();
