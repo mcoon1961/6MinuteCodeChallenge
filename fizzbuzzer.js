b0064bfb40a4ed0819f68220e8dec170fc7e5469
Declan Michaels <declan.michaels.actor@gmail.com>
2023-10-10 20:08:29 -0500

    cleaned up extra white space

--- fizzbuzzer.js
+++ fizzbuzzer.js
@@ -1,13 +1,8 @@
-function fizzbuzzer(entry) {
-
-    if (entry % 15 == 0)
-        return 'fizzbuzz';
-    if (entry % 5 == 0)
-        return 'buzz';
-    if (entry % 3 == 0)
-        return 'fizz';
-
-     return entry;
+function fizzbuzzer(entry) 
+{
+    if (entry % 15 == 0) return 'fizzbuzz';
+    if (entry % 5 == 0) return 'buzz';
+    if (entry % 3 == 0) return 'fizz';
+    return entry;
 }
-
 module.exports = fizzbuzzer
