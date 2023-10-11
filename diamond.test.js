cddd0d4c5887e50d8d737959d25d81b275a188ad
Declan Michaels <declan.michaels.actor@gmail.com>
2023-10-10 22:01:20 -0500

    changed test parameters

--- diamond.test.js
+++ diamond.test.js
@@ -1,5 +1,5 @@
 const diamond = require('./diamond');
 
 test('Letter A should be indented the number of spaces that the given letter appears in the alphabet', () => {
-    expect(diamond("A")).toBe(1)
+    expect(diamond("J")).toBe(10)
 });
